23300a973c41bdf972f98c0d6498e7590a51fa03
AeTher97 <michael93509@gmail.com>
2017-10-12 21:19:36 +0200

    working browser initialization and login scripts

--- OgameBot.py
+++ OgameBot.py
@@ -2,24 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 class OgameBot:
-    def _GetInfoResources(self):
+    def _getInfoResources(self):
         pass
-    def _GetInfoBuildings(self):
+
+    def _getInfoBuildings(self):
         pass
-    def _GetInfoTechnology(self):
+
+    def _getInfoTechnology(self):
         pass
-    def _GetInfoSizeOfPlanet(self):
+
+    def _getInfoSizeOfPlanet(self):
         pass
-    def _GetInfoPlanetNumber(self):
+
+    def _getInfoPlanetNumber(self):
         pass
-    def _LaunchBrowser(self):
-        pass
-    def _Login(self):
-        pass
-    def ChangeScope(self):
+
+    def _launchBrowser(self):
+        self.browser = webdriver.Chrome()
+        self.browser.get(('https://pl.ogame.gameforge.com/'))
+
+    def _login(self,login,password,universe):
+        commercialCloseButton = self.browser.find_element(By.XPATH,"//a[@href='javascript:;']")
+        LoginWindowOpen = self.browser.find_element_by_id('loginBtn')
+        usernameField = self.browser.find_element_by_id('usernameLogin')
+        passwordField = self.browser.find_element_by_id('passwordLogin')
+        serverField = self.browser.find_element_by_id('serverLogin')
+        loginButton = self.browser.find_element_by_id('loginSubmit')
+
+        commercialCloseButton.click()
+        LoginWindowOpen.click()
+        usernameField.send_keys(login)
+        passwordField.send_keys(password)
+        serverField.send_keys(universe)
+        loginButton.click()
+
+    def _botWait(self):
+        time.sleep(1)
+
+    def changeScope(self):
         pass
 
     # TODO
+
+
+
+"""login: michael93509@gmail.com password: oOunv72Pg744nd2d45zo: universe: Uriel"""
