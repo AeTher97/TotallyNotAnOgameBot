36cb5eb488d86cb4e327e23ead15aac4d935c594
AeTher97 <michael93509@gmail.com>
2017-10-12 20:38:22 +0200

    Added classes names

--- OgameBot.py
+++ OgameBot.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class OgameBot:
-    pass
+    def _GetInfoResources(self):
+        pass
+    def _GetInfoBuildings(self):
+        pass
+    def _GetInfoTechnology(self):
+        pass
+    def _GetInfoSizeOfPlanet(self):
+        pass
+    def _GetInfoPlanetNumber(self):
+        pass
+    def ChangeScope(self):
+        pass
+
     # TODO
