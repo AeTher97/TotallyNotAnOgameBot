16aa2272fbf22945e2f52a21e716265ef8c7fa48
MarekOchocki <marcopolo97@vp.pl>
2017-10-13 01:31:44 +0200

    Add PlanetState class

--- OgameBot.py
+++ OgameBot.py
@@ -4,23 +4,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
-
+from planetState import PlanetState
 
 class OgameBot:
+    def __init__(self):
+        self._resources = {'MetalMine': '1', 'CrystalMine': '2', 'DeuterExtractor': '3', 'SolarPowerPlant': '4',
+                           'FusionPowerPlant': '12', 'SolarSatellite': '212', 'MetalStorage': '22', 'CrystalStorage': '23',
+                           'DeuterStorage': '24'}
+        self._station = {'RobotFactory': '14', 'Shipyard': '21', 'Laboratory': '31', 'AllayDepot': '34', 'RocketSilo': '44',
+                         'NaniteFactory': '15', 'Terraformer': '33', 'SpaceDock': '36'}
 
-    _resources = {'MetalMine': '1', 'CrystalMine': '2', 'DeuterExtractor': '3', 'SolarPowerPlant': '4',
-                 'FusionPowerPlant': '12', 'SolarSatellite': '212', 'MetalStorage': '22', 'CrystalStorage': '23',
-                 'DeuterStorage': '24'}
-    _station = {'RobotFactory': '14', 'Shipyard': '21', 'Laboratory': '31', 'AllayDepot': '34', 'RocketSilo': '44',
-               'NaniteFactory': '15', 'Terraformer': '33', 'SpaceDock': '36'}
+        self._planetNumber = 0
+        self._planetSize = 0
+        self.current_scope = ""
 
-    _resourcesLevels = {'MetalMine': 0, 'CrystalMine': 0, 'DeuterExtractor': 0, 'SolarPowerPlant': 0,
-                       'FusionPowerPlant': 0, 'SolarSatellite': 0, 'MetalStorage': 0, 'CrystalStorage': 0,
-                       'DeuterStorage': 0}
-    _stationLevels = {'RobotFactory': 0, 'Shipyard': 0, 'Laboratory': 0, 'AllayDepot': 0, 'RocketSilo': 0,
-                     'NaniteFactory': 0, 'Terraformer': 0, 'SpaceDock': 0}
-    _planetNumber=0
-    _planetSize=0
+        self.mainPlanetState = PlanetState()
 
     def getInfoResources(self):
         self.setScope('overview')
@@ -28,11 +26,12 @@ class OgameBot:
         crystal = self.browser.find_element_by_id('resources_crystal').text
         deuter = self.browser.find_element_by_id('resources_deuterium').text
         energy = self.browser.find_element_by_id('resources_energy').text
-        print(metal)
-        print(crystal)
-        print(deuter)
-        print(energy)
-        
+
+        self.mainPlanetState.set("metal", metal)
+        self.mainPlanetState.set("crystal", crystal)
+        self.mainPlanetState.set("deuter", deuter)
+        self.mainPlanetState.set("energy", energy)
+
     def getInfoBuildings(self):
         self.setScope('resources')
         for building in self._resources:
@@ -44,7 +43,7 @@ class OgameBot:
                     EC.presence_of_all_elements_located((By.XPATH, "//*[@id='content']/div[2]/a")))
 
                 level = int(re.search(r'\d+', self.browser.find_element_by_xpath("//*[@id='content']/span").text).group())
-                self._resourcesLevels[building] = level
+                self.mainPlanetState.set(building, level)
 
         self.setScope('station')
         for building in self._station:
@@ -55,7 +54,7 @@ class OgameBot:
                 WebDriverWait(self.browser, 10).until(
                     EC.presence_of_all_elements_located((By.XPATH, "//*[@id='content']/div[2]/a/span")))
                 level = int(re.search(r'\d+',self.browser.find_element_by_xpath("//*[@id='content']/span").text).group())
-                self._stationLevels[building] = level
+                self.mainPlanetState.set(building, level)
             if building == 'Laboratory':
                 selection = "//a[@ref='" + self._station[building] + "']"
                 btnToClick = self.browser.find_element(By.XPATH, selection)
@@ -63,14 +62,7 @@ class OgameBot:
                 WebDriverWait(self.browser, 10).until(
                     EC.presence_of_all_elements_located((By.XPATH, "//*[@id='content']/div[2]/a")))
                 level = int(re.search(r'\d+',self.browser.find_element_by_xpath("//*[@id='content']/span").text).group())
-                self._stationLevels[building] = level
-
-        for element in self._resourcesLevels:
-            print(element)
-            print(self._resourcesLevels[element])
-        for element in self._stationLevels:
-            print(element)
-            print(self._stationLevels[element])
+                self.mainPlanetState.set(building, level)
 
     def getInfoTechnology(self):
         pass
@@ -120,7 +112,7 @@ class OgameBot:
         currentURL = self.browser.current_url
         uninumber = currentURL[9] + currentURL[10] + currentURL[11]
 
-        if (self.current_scope == page):
+        if self.current_scope == page:
             return
         if page == 'fleet':
             self.browser.get("https://s"+uninumber+"-pl.ogame.gameforge.com/game/index.php?page=fleet1")
---
+++ planetState.py
@@ -0,0 +1,66 @@
+
+
+class PlanetState:
+    def __init__(self):
+        self._attributes = ["metal",
+        "crystal",
+        "deuter",
+        "energy",
+
+        "MetalMine",
+        "CrystalMine",
+        "DeuterExtractor",
+        "SolarPowerPlant",
+        "FusionPowerPlant",
+        "SolarSatellite",
+        "MetalStorage",
+        "CrystalStorage",
+        "DeuterStorage",
+
+        "RobotFactory",
+        "Shipyard",
+        "Labolatory",
+        "AllayDepot",
+        "RocketSile",
+        "NaniteFactory",
+        "Terraformer",
+        "SpaceDock"]
+
+        for i in self._attributes:
+            setattr(self, i, 0)
+
+        # defenses
+        # ships
+        # technology
+
+    def set(self, attr_name, value):
+        """
+        :param attr_name: string
+        :param value: integer
+        :raises: ValueError if couldn't find attribute of given name
+        :return: True if everything went well
+        """
+        for i in self._attributes:
+            if attr_name == i:
+                setattr(self, attr_name, value)
+                return True
+        raise ValueError("There is no attribute named: " + str(attr_name))
+
+    def __str__(self):
+        result = str()
+        for i in self._attributes:
+            if(i != self._attributes[0]):
+                result += "\n"
+            result += i + " = " + str(getattr(self, i))
+        return result
+
+    def __ge__(self, other):
+        """
+        :param other: PlanetState object
+        :return: if every attribute is greater or equal return True, otherwise return False
+        """
+        for i in self._attributes:
+            if getattr(self, i) < getattr(other, i):
+                return False
+        return True
+
