404cbbfc0cc04fa3100795fe97324b11affd99a1
AeTher97 <michael93509@gmail.com>
2017-10-13 16:17:41 +0200

    PEP8 compatibile

--- OgameBot.py
+++ OgameBot.py
@@ -6,26 +6,68 @@ import time
 import re
 from planetState import PlanetState
 
+
 class OgameBot:
     def __init__(self):
-        self._resources = {'MetalMine': '1', 'CrystalMine': '2', 'DeuterExtractor': '3', 'SolarPowerPlant': '4',
-                           'FusionPowerPlant': '12', 'MetalStorage': '22', 'CrystalStorage': '23',
+        self._resources = {'MetalMine': '1',
+                           'CrystalMine': '2',
+                           'DeuterExtractor': '3',
+                           'SolarPowerPlant': '4',
+                           'FusionPowerPlant': '12',
+                           'MetalStorage': '22',
+                           'CrystalStorage': '23',
                            'DeuterStorage': '24'}
-        self._station = {'RobotFactory': '14', 'Shipyard': '21', 'Laboratory': '31', 'AllayDepot': '34', 'RocketSilo': '44',
-                         'NaniteFactory': '15', 'Terraformer': '33', 'SpaceDock': '36'}
+        self._station = {'RobotFactory': '14',
+                         'Shipyard': '21',
+                         'Laboratory': '31',
+                         'AllayDepot': '34',
+                         'RocketSilo': '44',
+                         'NaniteFactory': '15',
+                         'Terraformer': '33',
+                         'SpaceDock': '36'}
 
-        self._research = {'EnergyTechnology': '113', 'LaserTechnology':'120', 'IonTechnology':'121', 'HyperspaceTechnology': '114',
-                 'PlasmaTechnology': '122', 'CombustionDrive': '115', 'ImpulseDrive': '117', 'HyperDrive': '118',
-                 'SpyTechnology':'106','ComputerTechnology':'108','Astrophysics':'124','IntergalacticResearchNetwork':'123',
-                 'GravitonDevelopment':'199','BattleTechnology':'109','ShieldingTechnology':'110','Armor':'111'}
+        self._research = {'EnergyTechnology': '113',
+                          'LaserTechnology': '120',
+                          'IonTechnology': '121',
+                          'HyperspaceTechnology': '114',
+                          'PlasmaTechnology': '122',
+                          'CombustionDrive': '115',
+                          'ImpulseDrive': '117',
+                          'HyperDrive': '118',
+                          'SpyTechnology': '106',
+                          'ComputerTechnology': '108',
+                          'Astrophysics': '124',
+                          'IntergalacticResearchNetwork': '123',
+                          'GravitonDevelopment': '199',
+                          'BattleTechnology': '109',
+                          'ShieldingTechnology': '110',
+                          'Armor': '111'}
 
-        self._fleet = {'LightFigher': '204', 'HeavyFigher': '205', 'Cruiser': '206', 'Battleship': '207', 'LightTransport': '202',
-                       'HeavyTransport': '203', 'ColonizationShip': '208', 'Dreadnought': '215', 'Bomber': '211', 'Destroyer': '213',
-                       'DeathStar': '214', 'Recycler': '209', 'SpySatellite': '210', 'SolarSatellite': '212'}
+        self._fleet = {'LightFigher': '204',
+                       'HeavyFigher': '205',
+                       'Cruiser': '206',
+                       'Battleship': '207',
+                       'LightTransport': '202',
+                       'HeavyTransport': '203',
+                       'ColonizationShip': '208',
+                       'Dreadnought': '215',
+                       'Bomber': '211',
+                       'Destroyer': '213',
+                       'DeathStar': '214',
+                       'Recycler': '209',
+                       'SpySatellite': '210',
+                       'SolarSatellite': '212'}
 
-        self._defense = {'RocketLauncher': '401', 'LightLaserCannon': '402', 'HeavyLaserCannon': '403', 'GaussCannon': '404', 'IonCannon': '405',
-                         'PlasmaLauncher': '406', 'SmallPlanetaryShield': '407', 'LargePlanetaryShield': '408', 'AntiMissile': '502', 'InterplanetaryMissile': '503'}
-
+        self._defense = {'RocketLauncher': '401',
+                         'LightLaserCannon': '402',
+                         'HeavyLaserCannon': '403',
+                         'GaussCannon': '404',
+                         'IonCannon': '405',
+                         'PlasmaLauncher': '406',
+                         'SmallPlanetaryShield': '407',
+                         'LargePlanetaryShield': '408',
+                         'AntiMissile': '502',
+                         'InterplanetaryMissile': '503'}
 
         self.current_scope = ""
 
@@ -47,14 +89,15 @@ class OgameBot:
     def getInfoBuildings(self):
         self.setScope('resources')
         for building in self._resources:
-            if building!='SolarSatellite':
+            if building != 'SolarSatellite':
                 selection = "//a[@ref='" + self._resources[building] + "']"
                 btnToClick = self.browser.find_element(By.XPATH, selection)
                 btnToClick.click()
                 WebDriverWait(self.browser, 10).until(
                     EC.presence_of_all_elements_located((By.XPATH, "//*[@id='content']/div[2]/a")))
 
-                level = int(re.search(r'\d+', self.browser.find_element_by_xpath("//*[@id='content']/span").text).group())
+                level = int(re.search(r'\d+', self.browser.find_element_by_xpath(
+                    "//*[@id='content']/span").text).group())
                 self.mainPlanetState.set(building, level)
 
         self.setScope('station')
@@ -65,7 +108,7 @@ class OgameBot:
             btnToClick.click()
             WebDriverWait(self.browser, 10).until(
                 EC.presence_of_all_elements_located((By.XPATH, "//*[@id='content']/div[2]/a/span")))
-            level = int(re.search(r'\d+',self.browser.find_element_by_xpath("//*[@id='content']/span").text).group())
+            level = int(re.search(r'\d+', self.browser.find_element_by_xpath("//*[@id='content']/span").text).group())
             self.mainPlanetState.set(building, level)
 
 
@@ -109,12 +152,11 @@ class OgameBot:
             number = int(re.search(r'\d+', self.browser.find_element_by_xpath("//*[@id='content']/span").text).group())
             self.mainPlanetState.set(defense, number)
 
-
     def getInfoSizeOfPlanet(self):
         self.setScope('overview')
-        WebDriverWait(self.browser,10).until(
-            EC.presence_of_all_elements_located((By.XPATH,"//*[@id='diameterContentField']/span[2]")))
-        planetSize = self.browser.find_element(By.XPATH,"//*[@id='diameterContentField']/span[2]").text
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//*[@id='diameterContentField']/span[2]")))
+        planetSize = self.browser.find_element(By.XPATH, "//*[@id='diameterContentField']/span[2]").text
         self.mainPlanetState.set('PlanetSize', planetSize)
 
     def getInfoPlanetTemperature(self):
@@ -122,37 +164,35 @@ class OgameBot:
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_all_elements_located((By.XPATH, "//*[@id='honorContentField']")))
         self.botWait()
-        temperature = self.browser.find_element(By.XPATH,"//*[@id='temperatureContentField']").text
-        Edges = re.findall(r'\d+',temperature)
-        if temperature[3]=='-':
-            Edges[0]='-'+Edges[0]
+        temperature = self.browser.find_element(By.XPATH, "//*[@id='temperatureContentField']").text
+        Edges = re.findall(r'\d+', temperature)
+        if temperature[3] == '-':
+            Edges[0] = '-'+Edges[0]
 
-        average=(int(Edges[0])+int(Edges[1]))/2
+        average = (int(Edges[0])+int(Edges[1]))/2
         self.mainPlanetState.set('temperature', average)
 
     def getInfoPlanetPosition(self):
         self.setScope('overview')
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_all_elements_located((By.XPATH, "//*[@id='positionContentField']/a")))
-        positionString = self.browser.find_element(By.XPATH,"//*[@id='positionContentField']/a").text
+        positionString = self.browser.find_element(By.XPATH, "//*[@id='positionContentField']/a").text
 
-        numbers = re.findall(r'\d+',positionString)
-        self.mainPlanetState.set('Galaxy',numbers[0])
-        self.mainPlanetState.set('Star',numbers[1])
-        self.mainPlanetState.set('Planet',numbers[2])
-
+        numbers = re.findall(r'\d+', positionString)
+        self.mainPlanetState.set('Galaxy', numbers[0])
+        self.mainPlanetState.set('Star', numbers[1])
+        self.mainPlanetState.set('Planet', numbers[2])
 
     def logout(self):
         self.setScope('logout')
 
-
     def getInfoPlanetNumber(self):
         self.setScope('overview')
 
-        WebDriverWait(self.browser,10).until(
-            EC.presence_of_all_elements_located((By.XPATH,"//*[@id='countColonies']/p/span")))
-        string = self.browser.find_element(By.XPATH,"//*[@id='countColonies']/p/span").text
-        planetNumber=string[2]
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//*[@id='countColonies']/p/span")))
+        string = self.browser.find_element(By.XPATH, "//*[@id='countColonies']/p/span").text
+        planetNumber = string[2]
         self.planetNumber = planetNumber
 
     def launchBrowser(self):
@@ -183,7 +223,8 @@ class OgameBot:
 
     def setScope(self, page):
         """
-        :param page: string: on of these: overview, resources, station, traderOverwie, research, shipyard, defense, fleet, galaxy, highscore
+        :param page: string: on of these: overview, resources, station, traderOverwie, research, shipyard, defense,
+        fleet, galaxy, highscore
         :return: Nothing
         """
         currentURL = self.browser.current_url
@@ -197,7 +238,7 @@ class OgameBot:
             self.browser.get("https://s"+uninumber+"-pl.ogame.gameforge.com/game/index.php?page=" + page)
         self.current_scope = page
 
-    def build(self,*args):
+    def build(self, *args):
         thing = args[0]
 
         if len(args) == 1:
@@ -230,7 +271,7 @@ class OgameBot:
                 build = self.browser.find_element(By.XPATH, "//*[@id='content']/div[2]/a")
                 build.click()
 
-        if len(args)==2:
+        if len(args) == 2:
             number = args[1]
             if thing in self._fleet:
                 self.setScope('shipyard')
@@ -241,7 +282,7 @@ class OgameBot:
                     EC.presence_of_all_elements_located((By.ID, "number")))
                 numberField = self.browser.find_element_by_id('number')
                 numberField.send_keys(number)
-                build = self.browser.find_element(By.XPATH,"//*[@id='content']/div[3]/a/span")
+                build = self.browser.find_element(By.XPATH, "//*[@id='content']/div[3]/a/span")
                 build.click()
             if thing in self._defense:
                 self.setScope('defense')
@@ -255,20 +296,15 @@ class OgameBot:
                 build = self.browser.find_element(By.XPATH, "//*[@id='content']/div[3]/a/span")
                 build.click()
 
-    def SetSpySatelliteCount(self,number):
+    def SetSpySatelliteCount(self, number):
         self.setScope('preferences')
-        overall = self.browser.find_element(By.XPATH,'//*[@id="tabs-pref"]/li[2]')
+        overall = self.browser.find_element(By.XPATH, '//*[@id="tabs-pref"]/li[2]')
         overall.click()
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_all_elements_located((By.XPATH, "//*[@id='two']/div[2]/div/div/input")))
-        numberField = self.browser.find_element(By.XPATH,'//*[@id="two"]/div[2]/div/div/input')
+        numberField = self.browser.find_element(By.XPATH, '//*[@id="two"]/div[2]/div/div/input')
         numberField.send_keys(number)
         accept = self.browser.find_element_by_xpath("//*[@id='prefs']/div[1]/div[5]/input")
         accept.click()
 
-
-            # TODO
-
-
-
-
+        # TODO
